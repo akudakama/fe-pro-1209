bcadb50818c484370f95dfae93ee748d0e161402
akudakama <akugahao@gmail.com>
2024-09-22 20:40:02 +0300

    01

---
+++ 01/script/mine.js
@@ -0,0 +1,24 @@
+let usetNickname = 'akudakama';
+
+let userScore = 666;
+
+let age = 17;
+console.log(age);
+
+age = age + 5;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+isStudent = false;
+console.log(isStudent);
+
+let item = 10;
+console.log(item);
+
+item = "ten";
+console.log(item);
+
+item = true;
+console.log(item);
